5e6f9b8343532775f5b662f808674a00f8b3432d
Priyansh Jain <priyanshjain301@gmail.com>
2023-12-18 14:05:52 +0530

    Return model fields added

--- lib/constants/global_variables.dart
+++ lib/constants/global_variables.dart
@@ -10,7 +10,7 @@ import 'package:provider/provider.dart';
 //for android http://10.0.2.2:3000
 //for web http://127.0.0.1:3000
 //for ios http://localhost:3000
-//for external devices: Your pc IP (192.168.1.3). Run ipconfig/all inc cmd to find.
+//for external devices: Your pc IP (192.168.1.3). Run ipconfig /all inc cmd to find.
 String uri = 'http://192.168.29.168:3000';
 
 class GlobalVariables {
