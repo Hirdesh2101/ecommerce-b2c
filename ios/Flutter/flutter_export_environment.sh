e463c68c3496f553c624b15a3e4bcf4a3042fab8
Hirdesh2101 <hirdeshgarg0012@gmail.com>
2024-01-16 21:16:18 +0530

    ios taken old pub lock and resolved

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/priyanshjain/FlutterDev/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/priyanshjain/coding/AndroidStudioProjects/ecommerce-b2c"
+export "FLUTTER_ROOT=/Users/hirdesh/Development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/hirdesh/Desktop/projects/ecommerce-b2c"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
